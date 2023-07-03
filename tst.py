682129a7b89e3cef7691d007b0b2dda174537f6c
084uu <131406473+084uu@users.noreply.github.com>
2023-07-03 14:27:36 +0900

    Update and rename tst to tst.py

---
+++ tst.py
@@ -0,0 +1,12 @@
+# hallo world
+
+@bot.event
+async def on_raw_reaction_add(payload):
+    channel = bot.get_channel(payload.channel_id)
+    message = await channel.fetch_message(payload.message_id)
+    user = await bot.fetch_user(payload.user_id)
+    if isinstance(channel, discord.TextChannel):
+    
+    elif isinstance(channel, discord.DMChannel):
+
+
