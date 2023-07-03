13031a3aad7d55a45aa9529658f985de31ad702b
084uu <131406473+084uu@users.noreply.github.com>
2023-07-03 18:15:27 +0900

    Update tst.py

--- tst.py
+++ tst.py
@@ -17,7 +17,7 @@ async def on_raw_reaction_add(payload):
         if message.embeds[0]:
 			embed = message.embeds[0]
             if payload.emoji.name == '✅'
-    			if embed.title.startswith("会議を始めてください"):
+		if embed.title.startswith("会議を始めてください"):
     				# to 処刑
                     remain_vote_repeat = MAX_VOTE_REPEAT
                     
