365e3e5d0c8b5b8cbda0263320f7d279ec2cdaf9
084uu <131406473+084uu@users.noreply.github.com>
2023-07-03 17:38:07 +0900

    Update tst.py

--- tst.py
+++ tst.py
@@ -1,29 +1,44 @@
 # hallo world
 
+exit_flg = False
+remain_vote_repeat = 0
+
+MAX_VOTE_REPEAT = int(os.getenv(MAX_VOTE))
+
 @bot.event
 async def on_raw_reaction_add(payload):
     channel = bot.get_channel(payload.channel_id)
     message = await channel.fetch_message(payload.message_id)
     user = await bot.fetch_user(payload.user_id)
     if isinstance(channel, discord.TextChannel):
+		global remain_vote_repeat
         guild = await bot.fetch_guild(GUILD_ID)
         member = guild.get_member(payload.user_id)
         if message.embeds[0]:
 			embed = message.embeds[0]
-            if payload.emoji.name == '✅' and embed.title.startswith("投票先が決定しました"):
+			if payload.emoji.name == '✅' and embed.title.startswith("会議を始めてください"):
+				# to 処刑
+				remain_vote_repeat = MAX_VOTE_REPEAT
+
+				...
+				
+            elif payload.emoji.name == '✅' and embed.title.startswith("投票先が決定しました"):
                 await message.remove_reaction(payload.emoji, member)
                 pre_executed_ids = check_vote_max()
 				vote_dsc = get_vote_from_ids()
-                if len(pre_executed_ids) >= 2:
+                if len(pre_executed_ids) >= 2 and fin_vote != 0:
 					for pre_executed_id in pre_executed_ids:
 						pre_exer = await bot.fetch_user(pre_executed_id)
 						await pre_exer.send("処刑対象の候補になりました\n弁明の準備をしてください")
-                    embed.title = "決戦投票へ移ります"
-                    embed.description = f"投票結果\n{vote_dsc}\n \n弁明の時間に移ります"
-                    embed.set_footer(text="✅を押して進行してください")
-                    await message.edit(embed=embed)
-                    await message.add_reaction('✅')
-                else:
+					embed.title = "最多得票者が複数となりました"
+					embed.description = f"投票結果\n{vote_dsc}\n \n弁明の時間に移ります"
+					embed.set_footer(text="✅を押して進行してください")
+					await message.edit(embed=embed)
+					await message.add_reaction('✅')
+				else:
+					if len(pre_executed_ids) >= 2 and fin_vote == 0:
+						embed.title = "最多得票者が同率のためランダムで選択されます"
+						random.shuffle(pre_executed_ids)
                     executed_id = pre_executed_ids[0]
                     update_status(executed_id)
                     exer = await bot.fetch_user(executed_id)
@@ -34,7 +49,7 @@ async def on_raw_reaction_add(payload):
                     embed.set_footer(text="✅を押して進行してください")
                     await message.edit(embed=embed)
                     await message.add_reaction('✅')
-            elif payload.emoji.name == '✅' and embed.title.startswith("決戦投票へ移ります"):
+            elif payload.emoji.name == '✅' and embed.title.startswith("最多得票者が"):
                 await message.remove_reaction(payload.emoji, member)
 				await persuasion_tasks(message)
 			elif payload.emoji.name == '✅' and embed.title.startswith("処刑対象が決定しました"):
@@ -48,10 +63,11 @@ async def on_raw_reaction_add(payload):
                 await message.edit(embed=embed)
 			elif payload.emoji.name == '✅' and embed.title.startswith("弁明"):
 	            await message.remove_reaction(payload.emoji, member)
-				embed.title = "決戦投票を始めます"
+				embed.title = "決選投票を始めます"
 				embed.description = "LOADING" + "□"*count
                 embed.set_footer(text="しばらくお待ちください")
 				await message.edit(embed=embed)
+				await fin_vote_operates()
 
 			
 			...
@@ -62,6 +78,8 @@ async def on_raw_reaction_add(payload):
 			await message.delete()
 			exit_flg = True
 			await user.send("あなたの弁明がスキップされます")
+
+
         ...
 
 async def persuasion_operates(message):
@@ -156,7 +174,7 @@ async def persuasion_operates(message):
 		embed.set_footer(text= "次に移行します\nしばらくお待ちください")
 		await message.edit(embed=embed)
 	embed.description = "全ての弁明が終わりました"
-	embed.set_footer(text= "決戦投票を始めます\n✅を押して進行してください")
+	embed.set_footer(text= "決選投票を始めます\n✅を押して進行してください")
 	await message.edit(embed=embed)
 	await message.add_reaction('✅')
 	await clean_persuasion_dm(pre_executed_ids)
