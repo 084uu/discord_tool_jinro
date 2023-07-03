4c3514c11c0f2e1832421f9c81936d40919009de
084uu <131406473+084uu@users.noreply.github.com>
2023-07-03 15:54:18 +0900

    Update tst.py

--- tst.py
+++ tst.py
@@ -9,40 +9,44 @@ async def on_raw_reaction_add(payload):
         guild = await bot.fetch_guild(GUILD_ID)
         member = guild.get_member(payload.user_id)
         if message.embeds[0]:
-            embed = message.embeds[0]
+			embed = message.embeds[0]
             if payload.emoji.name == '✅' and embed.title.startswith("投票先が決定しました"):
                 await message.remove_reaction(payload.emoji, member)
-                executed_ids = check_vote_max()
-                if len(executed_ids) >= 2:
-                    vote_dsc = get_vote_from_ids(target_ids)
+                pre_executed_ids = check_vote_max()
+				vote_dsc = get_vote_from_ids()
+                if len(pre_executed_ids) >= 2:
+					for pre_executed_id in pre_executed_ids:
+						pre_exer = await bot.fetch_user(pre_executed_id)
+						await pre_exer.send("処刑対象の候補になりました\n弁明の準備をしてください")
                     embed.title = "決戦投票へ移ります"
                     embed.description = f"投票結果\n{vote_dsc}\n \n弁明の時間に移ります"
                     embed.set_footer(text="✅を押して進行してください")
                     await message.edit(embed=embed)
                     await message.add_reaction('✅')
                 else:
-                    executed_id = executed_ids[0]
+                    executed_id = pre_executed_ids[0]
                     update_status(executed_id)
                     exer = await bot.fetch_user(executed_id)
                     await exer.send("あなたは処刑される事となりました\n遺言を残してください")
                     exer_name = get_name_by_id(executed_id)
                     embed.title = "処刑対象が決定しました"
-                    embed.description = f"{exer_name}が処刑されることになりました\n遺言の時間に移ります"
+                    embed.description = f"投票結果\n{vote_dsc}\n \n{exer_name}が処刑されることになりました\n遺言の時間に移ります"
                     embed.set_footer(text="✅を押して進行してください")
                     await message.edit(embed=embed)
                     await message.add_reaction('✅')
             elif payload.emoji.name == '✅' and embed.title.startswith("決戦投票へ移ります"):
                 await message.remove_reaction(payload.emoji, member)
-                executed_ids = check_vote_max()
-            elif payload.emoji.name == '✅' and embed.title.startswith("処刑対象が決定しました"):
+				await persuasion_tasks(message)
+			elif payload.emoji.name == '✅' and embed.title.startswith("処刑対象が決定しました"):
                 await message.remove_reaction(payload.emoji, member)
-                will_dsc = embed.description
-                will_dsc = will_dsc.split("が処刑され")[0]
-                embed.description = f"{will_dsc}の遺言です"
-                embed.set_footer(text="遺言時間は1分です\nまもなく始まります")
+                await will_tasks(message)
+            elif payload.emoji.name == '✅' and embed.title.startswith("遺言"):
+				await message.remove_reaction(payload.emoji, member)
+				embed.title = "おそろしい夜がやってきました"
+				embed.description = "夜の行動を選択中です"
+                embed.set_footer(text="しばらくお待ちください")
                 await message.edit(embed=embed)
-                await will_task()
-            
+				
             
             ...
     
@@ -51,7 +55,15 @@ async def on_raw_reaction_add(payload):
         ...
         
 async def will_operates()
-    executed_id = get_exe_id_sham()
+	will_name = embed.description.split("が処刑され")[0]
+	embed.title = "遺言の時間です"
+	embed.description = f"{will_name}の遺言です"
+	embed.set_footer(text="遺言時間は1分です\nまもなく始まります")
+	await message.edit(embed=embed)
+
+	...
+	
+	executed_id = get_exe_id_sham()
 
     ...
 
